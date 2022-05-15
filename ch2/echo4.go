42886d7182128fdf8b57e87a45f4605a79db7576
YueXian <251682553@qq.com>
2022-05-15 11:51:31 +0800

    指针代码

--- ch2/echo4.go
+++ ch2/echo4.go
@@ -10,6 +10,17 @@ var n = flag.Bool("n", false, "omit trailing newline")
 var sep = flag.String("s", " ", "separator")
 
 func main() {
+	//x := 1
+	//println(x)
+	//p := &x
+	//println(p)
+	//println(&x)
+	//
+	//println(*p)
+	//*p = 2
+	//println(*p)
+	//println(x)
+
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
 	if !*n {
